1aced318c7ff64431e1f6e0af65f43e929261de6
Doppio JP <jakub@doppio.jp>
2014-02-01 12:16:38 +0000

    Init RSpec + basic story

---
+++ spec/acceptance/_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+feature 'Find the favourite language of the Geek on GitHub', %q{
+  As a curious person 
+  When I give the geek's GitHub user name
+  I would like to know
+  The favourite programming language of that geek.
+} do
+
+  scenario "Viewing the form" do
+    pending
+  end
+
+  scenario "Submitting geek's GitHub user name" do
+    pending
+  end
+
+  scenario "Viewing the favourite programming language" do
+    pending
+  end
+end
