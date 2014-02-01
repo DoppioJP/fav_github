e5ba731ee902865ebe5859d6b5ebb3f5c6245fca
Doppio JP <jakub@doppio.jp>
2014-02-01 11:41:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FavGithub::Application.config.secret_token = '237809dfcb95647d3795674cfdb5f94ad00074f9345e3d8171a31f137ed0fa7dca7604dbd3f59d783b9d4d94755f178612c4f426ca70d7e0224c812acfad9ae4'
