d91531d10c9a8495916a4950622ca5fd01b35275
Doppio JP <jakub@doppio.jp>
2014-02-02 14:50:56 +0000

    Deal with not existing users on Github

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,18 @@ class User
   
   def on_github?
     github = Github.new
+    github.user = @username
     true
   end
   
   def languages
     github = Github.new
-    repos_list = github.repos.list user: @username
-    repos_list.group_by(&:language)
-    repos_list.group_by(&:language).collect { |lang, repos| { lang: lang, count: repos.size } }
+    begin
+      repos_list = github.repos.list user: @username
+      repos_list.group_by(&:language)
+      repos_list.group_by(&:language).collect { |lang, repos| { lang: lang, count: repos.size } }
+    rescue
+      []
+    end
   end
 end
